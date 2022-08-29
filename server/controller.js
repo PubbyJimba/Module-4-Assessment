a82a303f01c92878ff62f9b9d0b21abba091733a
Isaac Sant <isaacjosephsant@gmail.com>
2022-08-29 13:28:28 -0600

    First commit

---
+++ server/controller.js
@@ -0,0 +1,29 @@
+module.exports = {
+  getCompliment: (req, res) => {
+    const compliments = [
+      "Gee, you're a smart cookie!",
+      "Cool shirt!",
+      "Your Javascript skills are stellar.",
+    ];
+
+    // choose random compliment
+    let randomIndex = Math.floor(Math.random() * compliments.length);
+    let randomCompliment = compliments[randomIndex];
+
+    res.status(200).send(randomCompliment);
+  },
+  getFortune: (req, res) => {
+    const fortunes = [
+      "A soft voice may be awfully persuasive.",
+      "Advice, when most needed, is least heeded.",
+      "At the touch of love, everyone becomes a poet.",
+      "Believe in yourself and others will too.",
+      "Believe it can be done.",
+    ];
+
+    let randomIndex = Math.floor(Math.random() * fortunes.length);
+    let randomFortune = fortunes[randomIndex];
+
+    res.status(200).send(randomFortune);
+  },
+};
